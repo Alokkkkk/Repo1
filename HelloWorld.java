479c02578989fb0ab64e69c227ce9b1b5a7d51eb
alok priyadarshi <alok.priyadarshi.met14@itbhu.ac.in>
2018-08-24 10:09:30 +0530

    updated file

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+class ByeWorld
+{
+
+public static void main(String []args)
+{
+
+System.out.println("Bye From Java");
+
+}
+
+
+
+}
+
+
---
+++ MyClass.java
@@ -0,0 +1,18 @@
+public class MyClass
+{
+static void main(String[]abcd)
+{
+System.out.println("MyClass");
+}
+}
+
+
+
+
+class YourClass
+{
+public static void main(String[]args)
+{
+System.out.println("YourClass");
+}
+}
